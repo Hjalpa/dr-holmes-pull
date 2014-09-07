df640dffa459e9a86e51af239a52bc3e0aed1312
tjwls0404 <tjwls0404@hotmail.com>
2014-09-06 22:48:25 -0400

    Create Juke with Nyan Cat.user.js

---
+++ Juke with Nyan Cat.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name          Juke with Nyan Cat
+// @namespace     Dr. Holmes
+// @description   Plays Nyan Cat when flag is grabbed
+// @include       http://tagpro-*.koalabeast.com*
+// @license       WTFPL
+// @author        Dr. Holmes
+// @version       2.2
+// ==/UserScript==
+ 
+nyanMusic = [
+    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
+    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
+    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
+    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
+    "http://k007.kiwi6.com/hotlink/z3cqmvyocq/retarded_nyan.mp3"
+    ]
+
+tempNyan = nyanMusic[0]
+nyan = document.createElement('audio');
+nyan.setAttribute('id', 'nyan');
+nyan.setAttribute('preload', 'auto');
+nyan.setAttribute('loop', '');
+assets.appendChild(nyan)
+nyan.volume = 0.6
+nyan.innerHTML = '<source src="' + tempNyan + '" type="audio/' + tempNyan.split('.').pop() + '">';
+ 
+function nyanSound() {
+    var flag = tagpro.players[tagpro.playerId].flag
+    if (flag) {
+        var nyanCat = document.getElementById('nyan');
+        var musicth = document.getElementById('music');
+        var rand = Math.floor(Math.random() * (5));
+        
+        if (document.getElementById('soundMusic').getAttribute('class') !== 'off') {
+            musicth.pause();
+        
+        
+       		nyanCat.innerHTML = '<source src="' + nyanMusic[rand] + '" type="audio/' + nyanMusic[rand].split('.').pop() + '">';
+        
+       		nyanCat.pause();
+        	nyanCat.load();
+        	nyanCat.play();
+        };
+    }
+}
+ 
+function nyanOff() {
+    var nyanCat = document.getElementById('nyan');
+    var musicth = document.getElementById('music');
+    nyanCat.pause();
+        if (document.getElementById('soundMusic').getAttribute('class') !== 'off') {
+        musicth.play();
+    }
+}
+ 
+ 
+tagpro.socket.on("sound", function(message) {
+    sound = message.s
+    if (["friendlyalert","placeholder"].indexOf(sound)>-1) {
+        setTimeout(function(){
+        nyanSound();}, 30)
+       
+    } else if (["friendlydrop","placeholder"].indexOf(sound)>-1) {
+        nyanOff();
+    } else if (["cheering","placeholder"].indexOf(sound)>-1) {
+        nyanOff();
+    }
+});

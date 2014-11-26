6b4bffe71c66d3f90e97691aec473f2c26130998
tjwls0404 <tjwls0404@hotmail.com>
2014-11-26 05:39:23 -0500

    Update Juke with Nyan Cat 2.3.user.js

--- Juke with Nyan Cat 2.3.user.js
+++ Juke with Nyan Cat 2.3.user.js
@@ -78,7 +78,7 @@ tagpro.ready(function(){
 	
 	else {
 		// When codition is pups
-		setInterval(function(){
+		setTimeout(function(){
 			tagpro.socket.on("p" ,function(message){
 				if (soundCondition == "pups"){
 					var nyan = document.getElementById("nyan");

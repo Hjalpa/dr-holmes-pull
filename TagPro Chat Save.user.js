fd35921495665a38cd3621f291ce719eebea15a2
tjwls0404 <tjwls0404@hotmail.com>
2014-12-13 20:22:32 -0500

    Update TagPro Chat Save.user.js

--- TagPro Chat Save.user.js
+++ TagPro Chat Save.user.js
@@ -7,24 +7,31 @@
 // @version       0.1
 // ==/UserScript==
 
-exit_key = 0;
+exit_key = 0; // <---- REPLACE '0' WITH A KEYCODE FOR A KEY OF YOUR CHOICE
 
 tagpro.ready(function(){
     var chat = [
         ["all",false,""],
         ["team",false,""],
         ["group",false,""]
-    ],
-        exitKey = [37,38,39,40,exit_key];
+        ],
+        
+        exitKey = [37,38,39,40,exit_key],
+        
+        all = tagpro.keys.chatToAll,
+        team = tagpro.keys.chatToTeam,
+        group = tagpro.keys.chatToGroup,
+        
+        $input = $("#chat");
     
     $(document).keydown(function(e){
 
-        if ($("#chat").val().length > 0){
+        if ($input.val().length > 0){
             chatSave(chat);
             
             if (exitKey.indexOf(e.which) > -1){
-                if ($("#chat").css("display")!="none"){
-                    $("#chat").val("");
+                if ($input.css("display")!="none"){
+                    $input.val("");
                     
                     var enter = $.Event("keydown", { keyCode: 13});
                     $("body").trigger(enter);
@@ -35,18 +42,19 @@ tagpro.ready(function(){
                     chat[2][1] = false;
                 }
             }
-            else if (e.which == 13){
+            else if (all.indexOf(e.which) > -1){
                     reset(chat);
         	}
-		}
+	    }
         
-		else if (e.which == 13 || e.which == 84 || e.which == 71){
-            if ($("#chat").css("display")!="none"){
-                if (e.which == 13){
+		else if (all.indexOf(e.which) + team.indexOf(e.which) + group.indexOf(e.which) > -3){
+			
+            if ($input.css("display")!="none"){
+                if (all.indexOf(e.which) > -1){
                     chat[0][1] = true;
-                } else if (e.which == 84){
+                } else if (team.indexOf(e.which) > -1){
                     chat[1][1] = true;
-                } else if (e.which == 71){
+                } else if (group.indexOf(e.which) > -1){
                     chat[2][1] = true;
                 }
                 
@@ -57,37 +65,37 @@ tagpro.ready(function(){
             }
 		}
 	});
+
+	function chatSave(chat){
+	    if(chat[0][1]){
+	        chat[0][2] = $input.val();
+	    } 
+	    else if (chat[1][1]){
+	        chat[1][2] = $input.val();
+	    } 
+	    else if (chat[2][1]){
+	    	chat[2][2] = $input.val();
+	    }
+	}
+	
+	function loadSave(chat){
+	   	if (chat[0][1]){
+	        $input.val(chat[0][2]);
+	    } 
+	    else if (chat[1][1]){
+	        $input.val(chat[1][2]);
+	    } 
+	    else if (chat[2][1]){
+	        $input.val(chat[2][2]);
+	    }
+	}
+	
+	function reset(chat){
+	    for (i=0;i<3;i++){
+	        if (chat[i][1]){
+	            chat[i][1] = false;
+	            chat[i][2] ="";
+	        }
+	    }
+	}
 });
-
-function chatSave(chat){
-    if(chat[0][1]){
-        chat[0][2] = $("#chat").val();
-    } 
-    else if (chat[1][1]){
-        chat[1][2] = $("#chat").val();
-    } 
-    else if (chat[2][1]){
-    	chat[2][2] = $("#chat").val();
-    }
-}
-
-function loadSave(chat){
-    if (chat[0][1]){
-        $("#chat").val(chat[0][2]);
-    } 
-    else if (chat[1][1]){
-        $("#chat").val(chat[1][2]);
-    } 
-    else if (chat[2][1]){
-        $("#chat").val(chat[2][2]);
-    }
-}
-
-function reset(chat){
-    for (i=0;i<3;i++){
-        if (chat[i][1]){
-            chat[i][1] = false;
-            chat[i][2] ="";
-        }
-    }
-}

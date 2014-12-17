0304c525eafa3568497974024f60729ad866208b
tjwls0404 <tjwls0404@hotmail.com>
2014-12-17 00:42:31 -0500

    Create TagPro Leaderboard Rankings.user.js

---
+++ TagPro Leaderboard Rankings.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Leaderboard Rankings
+// @namespace     Dr. Holmes
+// @description   Adds leaderboard rankings on the profile page
+// @include       http://tagpro-*.koalabeast.com*
+// @author        Dr. Holmes
+// @version       1.0
+// ==/UserScript==
+
+$(document).ready(function(){

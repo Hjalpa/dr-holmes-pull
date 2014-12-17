e2021875d8f13878f5b608bf2c436bb03dedd2d8
tjwls0404 <tjwls0404@hotmail.com>
2014-12-17 05:19:27 -0500

    Update Juke with Nyan Cat.user.js

--- Juke with Nyan Cat.user.js
+++ Juke with Nyan Cat.user.js
@@ -1,125 +1,74 @@
 // ==UserScript==
-// @name          Juke with Nyan Cat
-// @description   Plays Nyan Cat when flag is grabbed and aslo does other crazy things
-// @include       http://tagpro-*.koalabeast.com*
+// @name          Leader Board Rankings
+// @namespace     Dr. Holmes
+// @description   Adds leader board rankings on the profile page
+// @include       http://tagpro-*.koalabeast.com/profile/*
 // @author        Dr. Holmes
-// @version       3.3
+// @version       1.0
 // ==/UserScript==
- 
-nyanMusic = [
-    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
-    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
-    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
-    "http://k007.kiwi6.com/hotlink/mfz86avv7x/nyan.mp3",
-    "http://k007.kiwi6.com/hotlink/z3cqmvyocq/retarded_nyan.mp3"
-    ]
 
-colors = [
-    ['orange', '255, 153, 0'],
-    ['yellow', '255, 255, 0'],
-    ['green', '68, 122, 66'],
-    ['lightGreen', '8, 255, 0'],
-    ['lightBlue', '0, 238, 255'],
-    ['blue', '34, 0, 255'],
-    ['purple', '179, 0, 255'],
-    ['pink', '255, 0, 217'],
-    ['red', '255, 0, 0']
-	]
-
-// Nyan Music
-tempNyan = nyanMusic[0]
-nyan = document.createElement('audio');
-nyan.setAttribute('id', 'nyan');
-nyan.setAttribute('preload', 'auto');
-nyan.setAttribute('loop', '');
-assets.appendChild(nyan)
-nyan.volume = 0.6
-nyan.innerHTML = '<source src="' + tempNyan + '" type="audio/' + tempNyan.split('.').pop() + '">';
-
-// Ball
-$("body").find("#sound").after('<img id="colors"></img>');
-img = document.getElementById('colors');
-img.setAttribute('src','http://i.imgur.com/i8styCM.gif');
-
-img.style.position = 'absolute';
-img.style.top = '48.65%';
-img.style.left = '49.33%';
-img.style.visibility = 'hidden';
-
-// Background
-$("body").find("#sound").after('<div class="backgroundClr"></div>');
-var backgroundCSS = {
-    height:"100%",
-    width:"100%",
-    position:"fixed",
-    background:"rgba(0,0,0,0)",
-    transition:"background 0.2s linear 0s"
-}
-$(".backgroundClr").css(backgroundCSS)
-
-// Script
-function nyanSound() {
-    var flag = tagpro.players[tagpro.playerId].flag
-    if (flag) {
-        var nyanCat = document.getElementById('nyan');
-        var musicth = document.getElementById('music');
-        var rand = Math.floor(Math.random() * (5));
+$(document).ready(function(){
+	var rankDay = [],
+		rankWeek = [],
+		rankMonth = [],
+		
+		$day = $('<div>'),
+		$week = $('<div>'),
+		$month = $('<div>'),
         
-        if (document.getElementById('soundMusic').getAttribute('class') !== 'off') {
-            musicth.pause();
-        	
-        };
-        
-      	nyanCat.innerHTML = '<source src="' + nyanMusic[rand] + '" type="audio/' + nyanMusic[rand].split('.').pop() + '">';
-        nyanCat.pause();
-       	nyanCat.load();
-       	nyanCat.play();
-    }
-}
- 
-function nyanOff() {
-    var nyanCat = document.getElementById('nyan');
-    var musicth = document.getElementById('music');
-    nyanCat.pause();
-        if (document.getElementById('soundMusic').getAttribute('class') !== 'off') {
-        musicth.play();
-    }
-}
- 
-function colorOn() {
-    var flag = tagpro.players[tagpro.playerId].flag
-    if (flag) {
-    	var img = document.getElementById('colors');
-        img.style.visibility = '';
-    };
-}
+		myName = 'yippie'//$('body h3:nth-child(2)').html().split('<div>')[0];
 
-function backgroundOn() {
-    setInterval(function() {
-        var flag = tagpro.players[tagpro.playerId].flag
-        if (flag) {
-            var randClr = Math.floor(Math.random() * (8));
-            $('.backgroundClr').css("background","rgba(" + colors[randClr][1] + ",0.25)")
+		
+	$day.load('/boards article #Day .board tbody', function(){
+		rankDay = getRank($day,rankDay);
+	});
+	$week.load('/boards article #Week .board tbody', function(){
+		rankWeek = getRank($week,rankWeek);
+	});
+	$month.load('/boards article #Month .board tbody', function(){
+		rankMonth = getRank($month,rankMonth);
+	});
+    
+    setTimeout(function(){
+        
+        if (rankDay || rankWeek || rankMonth){
+            var rankDayHTML = '<span class="rank">'+rankDay[0]+'</span><span class="plusminus"> [+'+rankDay[1]+'/-'+rankDay[2]+']</span>';
+            var rankWeekHTML = '<span class="rank">'+rankWeek[0]+'</span><span class="plusminus"> [+'+rankWeek[1]+'/-'+rankWeek[2]+']</span>';
+            var rankMonthHTML = '<span class="rank">'+rankMonth[0]+'</span><span class="plusminus"> [+'+rankMonth[1]+'/-'+rankMonth[2]+']</span>';
+            
+            $table.find('th:eq(1)').before('<th>Rank</th>');
+            $('table:eq(0) tr:eq(1) td:eq(0)').before('<td>'+rankDayHTML+'</td');
+            $('table:eq(0) tr:eq(2) td:eq(0)').before('<td class="alt">'+rankWeekHTML+'</td>');
+            $('table:eq(0) tr:eq(3) td:eq(0)').before('<td>'+rankMonthHTML+'</td>');
+            $('table:eq(0) tr:eq(4) td:eq(0)').before('<td class="alt"></td>');
+            
+            $('table:eq(0) .plusminus').css('fontSize', '60%');
+            $('table:eq(0) .rank').css('color', '#00FF00');
         }
-        else {
-            clearInterval();
-            $('.backgroundClr').css("background","rgba(0,0,0,0.2)")
-        }
-    }, 201);
-}
- 
-tagpro.socket.on("sound", function(message) {
-    sound = message.s
-    if (["friendlyalert","placeholder"].indexOf(sound)>-1) {
-        setTimeout(function(){nyanSound();}, 30);
-        setTimeout(function(){colorOn();}, 4090);
-        setTimeout(function(){backgroundOn();}, 3890);
-       
-    } else if (["friendlydrop","placeholder"].indexOf(sound)>-1) {
-        nyanOff();
-        img.style.visibility = 'hidden';
-    } else if (["cheering","placeholder"].indexOf(sound)>-1) {
-        nyanOff();
-        img.style.visibility = 'hidden';
-    }
+    },500);
+	
+    
+	function getRank(div,rank){
+		for (i=0; i<div.find('tr').length; i++){
+            var name = div.find('tr:eq('+i+') td:eq(1)').text();
+			if (name == myName){
+                var plus,
+                    minus,
+                	points;
+                
+                points = parseInt(div.find('tr:eq('+i+') td:eq(2)').text());
+                plus = parseInt(div.find('tr:eq('+(i-1)+') td:eq(2)').text()) -points;
+                minus = points - parseInt(div.find('tr:eq('+(i+1)+') td:eq(2)').text());
+                
+                rank = [];
+				rank.push(i);
+				rank.push(plus);
+				rank.push(minus);
+			}
+            else if (!rank[1]){
+                rank = false;
+            }
+		}
+        return rank;
+	}
 });

09434f6d593298a5c89e8139a444d79d35fa884d
tjwls0404 <tjwls0404@hotmail.com>
2015-01-22 03:51:54 -0500

    Update TagPro Add Contact a Mod Button.user.js

--- TagPro Add Contact a Mod Button.user.js
+++ TagPro Add Contact a Mod Button.user.js
@@ -1,18 +1,25 @@
 // ==UserScript==
-// @name          TagPro Add Contact a Mod Button
-// @namespace     Dr. Holmes
-// @description   Adds 'Contact a Mod' button on the homepage
+// @name          TagPro IRC Buttons
+// @namespace     Dr. Holmes and Moosen
+// @description   Adds 'Contact a Mod' and 'Chat with friends' buttons on the homepage
 // @include       http://tagpro-*.koalabeast.com*
-// @author        Dr. Holmes
+// @author        Dr. Holmes and Moosen
 // @version       0.1
 // ==/UserScript==
 
 $(document).ready(function(){
-	$ircButton = $('<a>');
-	$ircButton.attr('href','http://webchat.freenode.net/?channels=tpmods');
-	$ircButton.attr('target','_blank');
-	$ircButton.css('marginLeft','10px');
-	$ircButton.text('Contact a Mod');
+	var $friendsButton = $('<a>');
+	$friendsButton.attr('href','http://webchat.freenode.net/?channels=tagpro');
+	$friendsButton.attr('target','_blank');
+	$friendsButton.css('marginLeft','10px');
+	$friendsButton.text('Chat with Friends');
 	$('article div.section.smaller a[href="/settings"').css('marginRight','10px');
-	$('article div.section.smaller:eq(0)').append($ircButton);
+	$('article div.section.smaller:eq(0)').append($friendsButton);
+	
+	var $modButton = $('<a>');
+	$modButton.attr('href','http://webchat.freenode.net/?channels=tpmods');
+	$modButton.attr('target','_blank');
+	$modButton.css('marginLeft','20px');
+	$modButton.text('Contact a Mod');
+	$('article div.section.smaller:eq(0)').append($modButton);
 });

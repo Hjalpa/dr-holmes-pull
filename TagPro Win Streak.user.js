80522507306ac85d1fb39d79cc0ca06a660a5739
tjwls0404 <tjwls0404@hotmail.com>
2015-02-15 19:13:15 -0500

    Update TagPro Win Streak.user.js

--- TagPro Win Streak.user.js
+++ TagPro Win Streak.user.js
@@ -1,101 +1,93 @@
 // ==UserScript==
-// @name          TagPro Win Streaks
+// @name          TagPro Win Streak
 // @namespace     Dr. Holmes
 // @description   Win Streak
 // @include       http://tagpro-*.koalabeast.com*
 // @author        Dr. Holmes
-// @version       0.1
+// @version       0.2
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant GM_log
 // ==/UserScript==
 
-$(document).ready(function(){
+tagpro.ready(function(){
 	var winStreak = JSON.parse(GM_getValue('winStreak',JSON.stringify(false)));
-	
-	if (winStreak == false){
-		createObj();
-	};
-	
-	if (window.location.pathname.indexOf(':') > -1){
-		inGame();
-	}
-	else if (window.location.pathname.indexOf('profile') > -1){
-		profilePage();
-	}
-	
-	function inGame(){
-		tagpro.ready(function(){
-			var joinTime = new Date().getTime();
-			var streak = winStreak.streak;
-			
-			socket.on('time', function(message){
-				joinTime = message.startTime.getTime();
-			});
-			
-			function win(){
-				if (winStreak.streak >= 0){
-					winStreak.streak++;
-				}
-				else {
-					winStreak.streak = 0;
-				}
-			};
-			
-			function loss(){
-				if (winStreak.streak <= 0){
-					winStreak.streak--;
-				}
-				else {
-					winStreak.streak = 0;
-				}
-			};
-			
-			function highStreak(){
-				if (winStreak.streak > winStreak.high){
-					winStreak.high = winStreak.streak;
-				}
-				GM_setValue('winStreak', winStreak);
-			};
-			
-			var time = setInterval(function(){
-				if (new Date().getTime() > joinTime+30000){
-					loss();
-					highStreak();
-					clearInterval(time);
-				}
-			},1000)
-			
-			socket.on('end', function(message){
-				winStreak.streak = streak;
-				if (tagpro.score.r > tagpro.score.b){
-					if (tagpro.players[tagpro.playerId].team == 1) win();
-					else loss();
-				} 
-				else if (tagpro.score.b > tagpro.score.b){
-					if (tagpro.players[tagpro.playerId].team == 2) win();
-					else loss();
-				}
-				else {
-					winStreak.streak = 0;
-				}
-				
-				highStreak();
-			});
-
-		})
-	}
-	
-	function createObj(){
+    if (winStreak == false){
 		newObj = {
 			streak: 0,
 			high: 0
 		}
 		GM_setValue('winStreak', JSON.stringify(newObj));
 		winStreak = JSON.parse(GM_getValue('winStreak',JSON.stringify(false)));
-	}
+	};
 	
-	function profilePage(){
+	if (window.location.href.indexOf(':') > -1){
+        var joinTime;
+        
+        tagpro.socket.on('time', function(message){
+            joinTime = new Date().getTime();
+        });
+        
+        function win(streak){
+            if (streak >= 0){
+                return streak + 1;
+            }
+            else {
+                return 1;
+            }
+        };
+        
+        function loss(streak){
+            if (streak <= 0){
+                return streak -1;
+            }
+            else {
+                return -1;
+            }
+        };
+        
+        function highStreak(){
+            if (winStreak.streak > winStreak.high){
+                winStreak.high = winStreak.streak;
+            }
+        };
+        
+        var time = setInterval(function(){
+            if (new Date().getTime() > joinTime+30000){
+                var streak = loss(winStreak.streak);
+                var newObj = {
+                    streak: streak,
+                    high: winStreak.high
+                };
+                GM_setValue('winStreak', JSON.stringify(newObj));
+                tagpro.socket.on('end',countStreak);
+                clearInterval(time);
+            }
+        },1000)
+        
+        function countStreak(){
+            var streak;
+            if (tagpro.score.r > tagpro.score.b){
+                if (tagpro.players[tagpro.playerId].team == 1) streak = win(winStreak.streak);
+                else streak = loss(winStreak.streak);
+            } 
+            else if (tagpro.score.b > tagpro.score.b){
+                if (tagpro.players[tagpro.playerId].team == 2) streak = win(winStreak.streak);
+                else streak = loss(winStreak.streak);
+            }
+                else {
+                    streak = 0;
+                }
+            highStreak();
+            var newObj = {
+                streak: streak,
+                high: winStreak.high
+            }
+            GM_setValue('winStreak', JSON.stringify(newObj));
+        };
+	}
+
+	else if (window.location.pathname.indexOf('profile') > -1){
 		var $winStreak = $('<table class="board"><tbody><tr><td></td><td></td></tr></tbody></table>');
 		$winStreak.find('td:eq(0)').text('Win Streak: '+ winStreak.streak);
 		$winStreak.find('td:eq(1)').text('Highest Streak: '+ winStreak.high);
